99dd1769bf311b0a65a9cb48d30b76f0a749d9da
Tim <38327800+Str4ky@users.noreply.github.com>
2023-01-27 09:09:01 +0100

    Update delete_project.php

--- scripts/delete_project.php
+++ scripts/delete_project.php
@@ -6,14 +6,13 @@
     $requete = "DELETE FROM tasks  WHERE project_id = '$id'";
     $cnn->query($requete) or die(print_r($bdd->errorInfo()));
 
-    $requete2 = "DELETE FROM projects  WHERE id = '$id'";
-    $cnn->query($requete2) or die(print_r($bdd->errorInfo())));
+    $requete2 = "UPDATE notes SET associated_project = 0  WHERE associated_project = '$id'";
+    $cnn->query($requete2) or die(print_r($bdd->errorInfo()));
 
-    $requete3 = "UPDATE notes SET associated_project = 0 WHERE associated_project = '$id'";
-    $cnn->query($requete3) or die(print_r($bdd->errorInfo())));
+    $requete3 = "DELETE FROM projects  WHERE id = '$id'";
+    $cnn->query($requete3) or die(print_r($bdd->errorInfo()));
 
-    header('Location: ../projects');
+    header("Location: ../projects");
 ?>
-
 
 
